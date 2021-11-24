27559bae86c67e3f61b9d26c868f63ce9c43140d
Alfonso Sánchez <sanchez4626@gmail.com>
2021-11-24 21:25:12 +0100

    calculadora

---
+++ operaciones.py
@@ -0,0 +1,25 @@
+import math
+
+
+def suma(numero1, numero2):
+    return numero1 + numero2
+
+
+def resta(numero1, numero2):
+    return numero1 - numero2
+
+
+def multi(numero1, numero2):
+    return numero1 * numero2
+
+
+def div(numero1, numero2):
+    return numero1 / numero2
+
+
+def factorial(factorial):
+    return math.factorial(factorial)
+
+
+def raiz(numeroraiz):
+    return math.sqrt(numeroraiz)
---
+++ test_calculadora.py
@@ -0,0 +1,9 @@
+import operaciones
+
+def test_pruebas():
+    assert operaciones.suma(3, 3) == 6
+    assert operaciones.resta(2, 1) == 1
+    assert operaciones.multi(3, 4) == 12
+    assert operaciones.div(10, 5) == 2
+    assert operaciones.raiz(25) == 5
+    assert operaciones.factorial(5) == 120
